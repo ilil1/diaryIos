a8dfef163279f383cecb98c4d33bcc1e5d0895a7
쓰리원 <90826931+ilil1@users.noreply.github.com>
2023-06-10 16:23:08 +0900

    UITabBarController 추가

---
+++ diary/DiaryCell.swift
@@ -0,0 +1,12 @@
+//
+//  DiaryCell.swift
+//  diary
+//
+//  Created by logispot on 2023/06/10.
+//
+
+import UIKit
+
+class DiaryCell: UICollectionViewCell {
+    
+}
---
+++ diary/StarCell.swift
@@ -0,0 +1,12 @@
+//
+//  StarCell.swift
+//  diary
+//
+//  Created by logispot on 2023/06/10.
+//
+
+import UIKit
+
+class StarCell: UICollectionViewCell {
+    
+}
